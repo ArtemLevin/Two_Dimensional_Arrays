f8dc31f46f425aab317dbe1b5445abc0e4d2414e
ArtemLevin <python_study_2022@mail.ru>
2022-12-15 12:19:23 +0300

    TWA

---
+++ Popov.66.1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Отсортировать по возрастанию элементов
+// последней строки целочисленный двухмерный массив 3×4.
+
+
+int[,] matrix = new int [3,4];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i,j] = new Random().Next(0,10);
+        Console.Write(matrix[i, j]+ " ");
+    }
+    Console.WriteLine(" ");
+}
