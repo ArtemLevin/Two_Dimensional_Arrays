627c6d4061a9d49abe4b9296c1e71dbffff05ccb
ArtemLevin <python_study_2022@mail.ru>
2022-12-15 13:21:09 +0300

    Sorting the line

--- Popov.66.1/Program.cs
+++ Popov.66.1/Program.cs
@@ -1,15 +1,84 @@
 ﻿// Отсортировать по возрастанию элементов
 // последней строки целочисленный двухмерный массив 3×4.
 
+Console.Write("Computer selectes the number of lines... ");
+int lines = new Random().Next(5, 10);
+Console.WriteLine(" ");
+Console.Write("Computer selectes the number of rows... ");
+int rows = new Random().Next(5, 10);
+Console.WriteLine(" ");
 
-int[,] matrix = new int [3,4];
+Console.Write("Computer creates an array... ");
+// Console.WriteLine(" ");
 
-for (int i = 0; i < matrix.GetLength(0); i++)
+int[,] CreateTwoDimArray(int lines, int rows)
 {
-    for (int j = 0; j < matrix.GetLength(1); j++)
+    int[,] matrix = new int[lines, rows];
+    for (int i = 0; i < matrix.GetLength(0); i++)
     {
-        matrix[i,j] = new Random().Next(0,10);
-        Console.Write(matrix[i, j]+ " ");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+        Console.WriteLine(" ");
     }
-    Console.WriteLine(" ");
+    return matrix;
 }
+void PrintTwoDimArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+int[,] matrix = new int[lines, rows];
+matrix = CreateTwoDimArray(lines, rows);
+
+int[,] AscendOrder(int[,] matrix)
+{
+    int i = lines-1;
+    int max = matrix[i, 0];
+    int temp;
+    int j = 0;
+    int k = 0;
+    while (j < rows)
+    {
+        max = matrix[i, j];
+        int p = j + 1;
+
+        while (p < rows)
+        {
+            if (matrix[i, p] > max)
+            {
+                max = matrix[i, p];
+                k = p;
+            }
+            p++;
+        }
+        if (j != rows-1 && matrix[i, j] < max)
+        {
+            temp = matrix[i, k];
+            matrix[i, k] = matrix[i, j];
+            matrix[i, j] = temp;
+        }
+
+        j++;
+
+    }
+    return matrix;
+}
+
+CreateTwoDimArray(lines, rows);
+PrintTwoDimArray(matrix);
+Console.Write("Computer prints the array... ");
+Console.WriteLine(" ");
+Console.Write("Computer sorts in ascending order the last line in the array... ");
+Console.WriteLine(" ");
+Console.Write("The computer prints the modified array... ");
+Console.WriteLine(" ");
+PrintTwoDimArray(AscendOrder(matrix));
