2eb5c187863df2d92dc1314cf8d8f80e3f77b794
ArtemLevin <python_study_2022@mail.ru>
2022-12-20 23:56:24 +0300

    counterclockwise rotation

---
+++ Popov.68.7/Program.cs
@@ -0,0 +1,48 @@
+﻿// Распределение чисел  по спирали против часовой стрелки в массиве.
+
+
+Console.Write("Enter the spiral size ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] SpiralArray(int n)
+{
+    int i = n-1, j = n;
+    int count = 0;
+    int p = 1;
+    int q = 0;
+    int u = 0;
+    int[,] array = new int[n, n];
+    while (true)
+    {      
+        while (u < n - q)
+        {
+            if (n * n <= count) return array;
+            if (p == 1) j--; 
+            if (p == 2) i--;
+            if (p == 3) j++;
+            if (p == 4) i++;
+            array[i, j] = n * n - count;
+            count++;
+            u++;
+        }
+        if(p==1 || p==3) q++;
+        if(p==4) p = 0;
+        u = 0;
+        p++;
+    }
+    return array;
+}
+
+void PrintTwoDimArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write("{0}\t", matrix[i, j]);
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+PrintTwoDimArray(SpiralArray(n));
