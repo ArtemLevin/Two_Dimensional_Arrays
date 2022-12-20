1232b2c7e861ddf0e9b9d8620f5be5d1d353de6a
ArtemLevin <python_study_2022@mail.ru>
2022-12-21 01:43:25 +0300

    Permutation of rows and columns

--- PermRowsInBackOder/Program.cs
+++ PermRowsInBackOder/Program.cs
@@ -1,25 +1,25 @@
 ﻿int[,] CreateArray(int rows, int columns)
 {
-int[,] array = new int[rows, columns];
-for (int i = 0; i < rows; i++)
-{
-for (int j = 0; j < columns; j++)
-{
-array[i, j] = new Random().Next(0, 11);
- }
-}
-return array;
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return array;
 }
 void PrintArray(int[,] array)
 {
-for (int i = 0; i < array.GetLength(0); i++)
-{
-for (int j = 0; j < array.GetLength(1); j++)
-{
-Console.Write("{0}	", array[i, j]);
-}
-Console.WriteLine(" ");
-}
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0}	", array[i, j]);
+        }
+        Console.WriteLine(" ");
+    }
 }
 Console.Write("Enter the number or rows...");
 int rows = int.Parse(Console.ReadLine()!);
@@ -35,38 +35,38 @@ Console.WriteLine(" ");
 
 int[,] PermutationRows(int[,] array)
 {
-    
-    for (int i = 0; i < (array.GetLength(0))/2; i++)
+
+    for (int i = 0; i < (array.GetLength(0)) / 2; i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             int temp = array[i, j];
-            array[i, j] = array[array.GetLength(0)-1-i, j];
-            array[array.GetLength(0)-1-i, j] = temp;
-        }  
+            array[i, j] = array[array.GetLength(0) - 1 - i, j];
+            array[array.GetLength(0) - 1 - i, j] = temp;
+        }
     }
     return array;
 }
 
 PrintArray(PermutationRows(array));
- 
+
 Console.WriteLine(" ");
 
 PrintArray(array);
-  
+
 Console.WriteLine(" ");
 
- int[,] PermutationColumns(int[,] array)
+int[,] PermutationColumns(int[,] array)
 {
-    
-    for (int j = 0; j < (array.GetLength(1))/2; j++)
+
+    for (int j = 0; j < (array.GetLength(1)) / 2; j++)
     {
         for (int i = 0; i < array.GetLength(0); i++)
         {
             int temp = array[i, j];
-            array[i, j] = array[i, array.GetLength(0)-1-j];
-            array[i, array.GetLength(0)-1-j] = temp;
-        }  
+            array[i, j] = array[i, array.GetLength(0) - 1 - j];
+            array[i, array.GetLength(0) - 1 - j] = temp;
+        }
     }
     return array;
 }
