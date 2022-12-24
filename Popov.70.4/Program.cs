5c6d1e9690b4e029b0c4180a7808bc317eee243d
ArtemLevin <python_study_2022@mail.ru>
2022-12-24 16:59:30 +0300

    Two to OneDim and sorted in ascend order

--- Popov.70.4/Program.cs
+++ Popov.70.4/Program.cs
@@ -64,3 +64,22 @@ int[] TwoDimToOneDim(int[,] array)
 
 PrintOneDimArray(TwoDimToOneDim(array));
 
+int[] SortingAscend (int[] array)
+{
+    for (int i = 1; i < array.Length; i++)
+    {
+        for (int j = 0; j < array.Length - i; j++)
+        {
+            if(array[j]>array[j+1])
+            {
+                int temp = array[j+1];
+                array[j+1] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
+}
+Console.WriteLine(" ");
+
+PrintOneDimArray(SortingAscend (TwoDimToOneDim(array)));
