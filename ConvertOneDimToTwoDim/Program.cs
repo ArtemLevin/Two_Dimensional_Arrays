696e2a88bd51993d1939f556a3497052456d9cbc
ArtemLevin <python_study_2022@mail.ru>
2022-12-25 00:51:37 +0300

    PascalTriangle

---
+++ ConvertOneDimToTwoDim/Program.cs
@@ -0,0 +1,81 @@
+﻿int[,] CreateArray(int rows, int columns)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(0, 11);
+        }
+    }
+    return array;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0}	", array[i, j]);
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+void PrintOneDimArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("{0}	", array[i]);
+    }
+    Console.WriteLine(" ");
+}
+
+Console.Write("Enter the number or rows...");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number or columns..");
+int columns = int.Parse(Console.ReadLine()!);
+Console.WriteLine(" ");
+int[,] array = new int[rows, columns];
+array = CreateArray(rows, columns);
+Console.WriteLine(" ");
+PrintArray(array);
+Console.WriteLine(" ");
+
+int[] TwoDimToOneDim(int[,] array)
+{
+    int index = 0;
+    int[] OneDimArray = new int[array.GetLength(0) * array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            OneDimArray[index] = array[i, j];
+            index++;
+        }
+    }
+    return OneDimArray;
+}
+
+PrintOneDimArray(TwoDimToOneDim(array));
+Console.WriteLine(" ");
+
+int[,] ConvertOneDimToTwoDimArray(int[] array)
+{
+    Console.Write("Enter the number of rows in new array ");
+    int newRows = int.Parse(Console.ReadLine()!);
+    int p = 0;
+    int[,] TwoDimArray = new int[newRows, (array.Length)/newRows];
+    for (int i = 0; i < TwoDimArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < TwoDimArray.GetLength(1); j++)
+        {
+            TwoDimArray[i, j] = array[p];
+            p++;
+        }
+    }
+    return TwoDimArray;
+}
+
+Console.WriteLine(" ");
+PrintArray(ConvertOneDimToTwoDimArray(TwoDimToOneDim(array)));
---
+++ PascalTriangle/Program.cs
@@ -0,0 +1,35 @@
+﻿// Console.WriteLine("Enter number of lines in triangles ");
+// int rows = int.Parse(Console.ReadLine()!);
+
+int[,] PascalTriangle(int rows)
+{
+    int columns = 2 * rows + 1;
+    int[,] triangle = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 1; j < columns-1; j++)
+        {
+            if (i == 0 && j == rows) triangle[i, j] = 1;
+            if (i > 0)
+            {
+                triangle[i, j] = triangle[i - 1, j - 1] + triangle[i - 1, j + 1];
+            }
+        }
+    }
+    return triangle;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0}	", array[i, j]);
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+PrintArray(PascalTriangle(6));
+
